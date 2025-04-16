fc4824dd71c142a3b4dfbf296bd3b45f504bfb07
Ben <bb14738@ms.wasatch.edu>
2025-04-16 09:24:23 -0600

    init

---
+++ DarknessExpansion/Darkness.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using R2API;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using CombatDirector = On.RoR2.CombatDirector;
+
+
+namespace DarknessExpansion;
+
+public class Darkness
+{
+    public static EliteDef DarknessElite;
+    public static EquipmentDef DarknessEquipment;
+    public static BuffDef DarknessBuff;
+    public static int DarknessLevel = 3;
+    private static Sprite eliteIcon = Addressables.LoadAssetAsync<Sprite>("RoR2/Base/EliteIce/texBuffAffixWhite.tif").WaitForCompletion();
+
+    public Darkness()
+    {
+        DarknessElite = ScriptableObject.CreateInstance<EliteDef>();
+        DarknessElite.color = new Color(0, 0, 0, 255);
+        EliteRamp.AddRamp(DarknessElite,CreateDarknessTexture());
+        DarknessElite.name = "EliteDarkness";
+
+        DarknessBuff = ScriptableObject.CreateInstance<BuffDef>();
+        DarknessBuff.name = "DarknessBuff";
+        DarknessBuff.canStack = false;
+        DarknessBuff.isCooldown = false;
+        DarknessBuff.isDebuff = false;
+        DarknessBuff.buffColor = new Color(0, 0, 0, 255);
+        DarknessBuff.iconSprite = eliteIcon;
+
+        DarknessEquipment = ScriptableObject.CreateInstance<EquipmentDef>();
+        DarknessEquipment.appearsInMultiPlayer = true;
+        DarknessEquipment.appearsInSinglePlayer = true;
+        DarknessEquipment.canBeRandomlyTriggered = false;
+        DarknessEquipment.canDrop = false;
+        DarknessEquipment.colorIndex = ColorCatalog.ColorIndex.Equipment;
+        DarknessEquipment.cooldown = 0.0f;
+        DarknessEquipment.isLunar = false;
+        DarknessEquipment.isBoss = false;
+        DarknessEquipment.passiveBuffDef = DarknessBuff;
+        DarknessEquipment.dropOnDeathChance = 0;
+        DarknessEquipment.enigmaCompatible = false;
+
+        DarknessElite.eliteEquipmentDef = DarknessEquipment;
+        DarknessBuff.eliteDef = DarknessElite;
+
+        ContentAddition.AddEliteDef(DarknessElite);
+        ContentAddition.AddBuffDef(DarknessBuff);
+        ContentAddition.AddEquipmentDef(DarknessEquipment);
+        
+        UpdateDarkness();
+        CombatDirector.Spawn += CombatDirectorOnSpawn;
+        SpawnCard.onSpawnedServerGlobal += SpawnCardOnonSpawnedServerGlobal;
+    }
+
+    private void SpawnCardOnonSpawnedServerGlobal(SpawnCard.SpawnResult obj)
+    {
+        Inventory i = obj.spawnedInstance.GetComponent<Inventory>();
+        if (i != null)
+        {
+            if (i.GetEquipmentIndex() == DarknessEquipment.equipmentIndex)
+            {
+                float itemChance = DarknessLevel / 10f;
+                if (Random.value < itemChance)
+                {
+                    List<ItemIndex> li = ItemCatalog.tier3ItemList;
+                    i.GiveItem(li[(int)(li.Count * Random.value)]);
+                    Log.Debug("Giving Item");
+                }
+            }
+        }
+
+    }
+
+    public static Texture2D CreateDarknessTexture()
+    {
+        Texture2D texture = new Texture2D(256, 8);
+        texture.wrapMode = TextureWrapMode.Clamp;
+
+        for (int y = 0; y < 8; y++)
+        {
+            for (int x = 0; x < 256; x++)
+            {
+                
+                texture.SetPixel(x, y, new Color(0,0,0,255));
+            }
+        }
+        
+        texture.Apply();
+        
+        return texture;
+    }
+
+
+    private bool CombatDirectorOnSpawn(CombatDirector.orig_Spawn orig, RoR2.CombatDirector self, SpawnCard spawncard, EliteDef elitedef, Transform spawntarget, DirectorCore.MonsterSpawnDistance spawndistance, bool preventoverhead, float valuemultiplier, DirectorPlacementRule.PlacementMode placementmode)
+    {
+        float chanceToSwap = DarknessLevel * DarknessLevel / 100f; 
+        if (Random.value < chanceToSwap)
+        {
+            elitedef = DarknessElite;
+            Log.Debug("Changing Elite Type");
+            
+        }
+
+        return orig(self,spawncard,elitedef,spawntarget,spawndistance,preventoverhead,valuemultiplier,placementmode);
+    }
+    
+    public void UpdateDarkness()
+    {
+        DarknessElite.healthBoostCoefficient = 1f + Mathf.Sqrt(DarknessLevel);
+        DarknessElite.damageBoostCoefficient = 1f + (DarknessElite.healthBoostCoefficient-1) / 2f;
+    }
+}
---
+++ DarknessExpansion/DarknessExpansion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BepInEx;
+using R2API;
+using UnityEngine;
+
+namespace DarknessExpansion;
+[BepInPlugin("com.cybug.DarknessExpansion", "DarknessExpansion","0.0.1")]
+[BepInDependency(ItemAPI.PluginGUID,BepInDependency.DependencyFlags.HardDependency)]
+[BepInDependency(EliteAPI.PluginGUID,BepInDependency.DependencyFlags.HardDependency)]
+
+public class DarknessExpansion : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Log.Init(Logger);
+        new Darkness();
+    }
+    
+}
